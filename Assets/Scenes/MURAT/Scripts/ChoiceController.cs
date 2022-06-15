b203aa770ecca319ce683b2e181b8b50e525a384
MuratEltturkey <murataltunkaya7@gmail.com>
2022-06-16 00:21:37 +0300

    AnaMenü ve karakter seçim ekranı hazırlığı

---
+++ Assets/Scenes/MURAT/Scripts/ChoiceController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceController : MonoBehaviour
+{
+    private void Start()
+    {
+        //ResourceLoad ile sahneleri tek tek buraya çaðýracaz.
+    }
+}
