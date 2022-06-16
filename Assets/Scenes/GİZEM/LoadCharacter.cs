7ef2d07c6e74261c42ae7b474f086d2620f3fb2b
Gizem BAL SARI <96011063+balgbal@users.noreply.github.com>
2022-06-16 14:53:05 +0300

    Anamenu ve buton denemeleri

---
+++ Assets/Scenes/GİZEM/LoadCharacter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadCharacter : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    public Transform spPoint;
+    public TMP_Text label;
+
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        GameObject clone = Instantiate(prefab, spPoint.position, Quaternion.identity);
+        label.text = prefab.name;
+
+
+    }
+
+}
