41ce38b8d472a299a86aadf385a39d1222c1fe1f
MuratEltturkey <murataltunkaya7@gmail.com>
2022-06-23 22:30:14 +0300

    Sahne hazırlıkları

---
+++ Assets/Scenes/MURAT/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Play()
+    {
+
+    }
+    public void Quit()
+    {
+
+    }
+    public void HowToPlay()
+    {
+
+    }
+    public void Options()
+    {
+
+    }
+}
